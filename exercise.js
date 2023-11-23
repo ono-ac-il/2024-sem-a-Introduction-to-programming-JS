4cbe101862a2c5127b8490621bac2ef12255b571
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2023-11-23 10:22:36 +0200

    added random

--- exercise.js
+++ exercise.js
@@ -1,7 +1,11 @@
-// var i= 1
-// loop 1 to 100
+var x = 15;
+
+console.log(16 % 3); // = 1
+console.log(8 % 2 == 0); // true
 // if i divides by 3 without remain -> console.log('fizz')
 // if i divides by 5 without remain -> console.log('buzz')
+
+// bonus:
 // if i divides by 3 and 5 without remain -> console.log('fizzbuzz')
 
 
---
+++ extra.js
@@ -0,0 +1,2 @@
+var rand = Math.floor(Math.random() * 100);
+console.log(rand)

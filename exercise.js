2ba258437e81bde0825b437def21d76b6a9ba467
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2023-11-23 09:37:58 +0200

    added exercise

---
+++ exercise.js
@@ -0,0 +1,8 @@
+// var i= 1
+// loop 1 to 100
+// if i divides by 3 without remain -> console.log('fizz')
+// if i divides by 5 without remain -> console.log('buzz')
+// if i divides by 3 and 5 without remain -> console.log('fizzbuzz')
+
+
+

2290133d035f443e40b74632f91473f47d38061f
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2023-11-23 09:21:01 +0200

    updated

---
+++ 3flow.js
@@ -0,0 +1,18 @@
+var a = 3;
+var b = 2;
+
+if (a > b) {
+    console.log("line", 5, a > b, a, "is bigger then", b);
+}
+
+if (a < b) {
+    console.log("line", 9, a < b, a, "is smaller then", b);
+}
+
+var age = 26;
+
+if (age > 30) {
+    console.log("20% discount");
+} else if (age > 20) {
+    console.log("10% discount");
+}

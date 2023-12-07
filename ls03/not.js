45abe1a73096d393159edd47fb9cece1ee459c38
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2023-12-07 10:23:32 +0200

    lesson3

---
+++ ls03/not.js
@@ -0,0 +1,9 @@
+// Not operator - !
+
+if (x > 5) {
+    console.log('x > 5', 'x bigger then 5', 'x not small then 5');
+}
+
+if (!(x < 5)) {
+    console.log('x > 5', 'x bigger then 5', 'x not small then 5');
+}
---
+++ ls03/numbers.js
@@ -0,0 +1,10 @@
+var x = 1;
+var y = 6;
+
+var z = 56.5656;
+
+console.log(x, y, z);
+
+console.log(0.01 + 0.02);
+
+console.log(0.1 + 0.2);

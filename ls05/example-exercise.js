97d9ce3a8f8d95705700b8208f53c9a08efffd3d
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2023-12-21 10:39:08 +0200

    example

---
+++ ls05/example-exercise.js
@@ -0,0 +1,22 @@
+var n = 20;
+
+console.log("=====sum of n ========");
+var sum = 1;
+while (sum < n) {
+    sum++;
+    var newSum = sum + sum;
+
+}
+console.log(newSum);
+
+
+var n = 20;
+var sum = 0;
+var counter = 1;
+
+while (counter < n) {
+    sum = sum + counter;
+    counter++;
+}
+console.log(sum);
+
